f08925461654e91465629847154227ab9c16f8b9
Jason Horsley <Jason@Pubbly.com>
2018-10-18 08:42:33 -0700

    Engine fix 1.1.1 DomInteractionCover max-height:100%

--- html/engine/1.1.1/js/run/domInteractionCover.js
+++ html/engine/1.1.1/js/run/domInteractionCover.js
@@ -7,7 +7,7 @@ class DomInteractionCover {
          style='position:absolute;height:${height}px;width:${width}px;background-color:white'> 
          <img src="${this.picked.relPath}"
          class='transformCenter cursor-pointer'
-         style='height: 100%' 
+         style='max-height: 100%' 
          loop=infinite />
          </div>`);
         this.cb = cb;
