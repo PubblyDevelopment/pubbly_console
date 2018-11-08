895ef8aea5731c0704901758a8f208e22221ab49
Jason Horsley <Jason@Pubbly.com>
2018-11-08 07:19:58 -0800

    Console: Added check for maps where if node has no paths it displays a warning (instead of null)

--- html/NavigationNodesUI/js/element_Dropdown.js
+++ html/NavigationNodesUI/js/element_Dropdown.js
@@ -4,35 +4,46 @@ class NavigationNodes_Dropdown extends NavigationNodes_element {
     }
 
     populateDropdown(node) {
-    	document.getElementById('firstNodeSelected').innerHTML = node.name;
-    	
-    	// Get select element
+        document.getElementById('firstNodeSelected').innerHTML = node.name;
+
+        // Get select element
         let select = document.getElementById("pathSelections");
-        
+
         // Empty options panel to prevent duplicates
         for (let o in select.options) {
             select.options.remove(o);
         }
+        // Enable by default (disabled if no paths, down like 5 lines)
+        select.removeAttribute("disabled");
 
-        for (let l in node.paths)
-            select.options[select.options.length] = new Option(node.paths[l].link_name);
+
+        for (let l in node.paths) {
+            let name = node.paths[l].link_name;
+            if (name) {
+                select.options[select.options.length] = new Option(name);
+            }
+        }
+        if (node.paths.length === 1 && !node.paths[0].link_name) {
+            select.options[select.options.length] = new Option("No paths for connection");
+            select.setAttribute("disabled", "true");
+        }
 
         return select.options.length;
-          
+
     }
 
-    getDropdownSelection () {
-    	// Get select element
+    getDropdownSelection() {
+        // Get select element
         let select = document.getElementById("pathSelections");
 
         return select.value;
     }
 
     setSecondNodeTitle(node) {
-    	document.getElementById('secondNodeSelected').innerHTML = node.name;
+        document.getElementById('secondNodeSelected').innerHTML = node.name;
     }
 
-    setDropdownSelection (selection) {
+    setDropdownSelection(selection) {
         let select = document.getElementById("pathSelections");
 
         for (let o in select.options) {
