ed03482434e5ebbb47e768ae1c5bb142ff583545
Jason Horsley <Jason@JasonHorsley.tech>
2019-02-18 14:36:25 -0500

    Removed ignored items from tree

--- html/js/branding.js
+++
@@ -1,5 +0,0 @@
-let brand = "FasTracKids";
-window.setTimeout(function() {
-	let title = document.head.getElementsByTagName("title")[0].innerHTML;
-	document.head.getElementsByTagName("title")[0].innerHTML = brand + " " + title;
-},1);
